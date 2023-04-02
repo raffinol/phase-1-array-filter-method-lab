d124da59dd36670f81e06eab439104c0cb4a4543
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-02 02:03:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// // Code your solution here
+
+function findMatching(drivers, names){
+    const result = drivers.filter(input => input.toLowerCase() === names.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, letters){
+    const result = drivers.filter(input => input.startsWith(letters) === true)
+    return result;
+}
+
+function matchName(drivers, names){
+    const result = drivers.filter(input => input.name === names)
+    return result;
+}
+
+
